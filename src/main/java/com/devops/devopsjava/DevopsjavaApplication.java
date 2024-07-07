b8a9a8a241a99d9278329a384ab79a437711c968
santoshkumarm1 <santoshkumarm1@verifone.com>
2024-07-07 07:34:04 +0530

    devops java.

---
+++ src/main/java/com/devops/devopsjava/DevopsjavaApplication.java
@@ -0,0 +1,13 @@
+package com.devops.devopsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/devopsjava/DevopsjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.devopsjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
